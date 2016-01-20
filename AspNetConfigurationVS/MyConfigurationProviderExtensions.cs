bb67d00a043e49e4d3b36d11b0b8de3963a37424
Jeff Ogata <jeffogata@outlook.com>
2016-01-19 21:46:32 -0600

    simplify extension method.

--- AspNetConfigurationVS/MyConfigurationProviderExtensions.cs
+++ AspNetConfigurationVS/MyConfigurationProviderExtensions.cs
@@ -6,9 +6,7 @@
     {
         public static IConfigurationBuilder AddMyConfiguration(this IConfigurationBuilder builder)
         {
-            builder.Add(new MyConfigurationProvider());
-
-            return builder;
+            return builder.Add(new MyConfigurationProvider());
         }
     }
 }
